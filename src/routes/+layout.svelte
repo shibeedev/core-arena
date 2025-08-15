e0f756d85614e191fb17dfa7975c99c984b275b7
Shibee <116005554+shibeedev@users.noreply.github.com>
2025-08-16 06:32:03 +0700

    Delete src directory

--- src/routes/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import "../app.css";
-    import 'driver.js/dist/driver.css';
-    import Analytics from '$lib/Analytics.svelte'
-</script>
-<Analytics />
-<div class="font-Oxanium">
-    <slot />
-</div>
